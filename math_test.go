c131bb17e7ecf3d052743436de5e9d110f2edfda
Wesley Westelley <wesleywestelley@gmail.com>
2022-10-03 18:31:12 -0300

    feat: add function to sum

---
+++ math_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestSum(t *testing.T) {
+	total := Sum(15, 15)
+
+	if total != 30 {
+		t.Errorf("Result of the sum is invalid: Result %d. Expected %d.", total, 30)
+	}
+}
